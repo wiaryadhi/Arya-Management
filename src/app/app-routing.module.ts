8024dae6a171657647ad5a9eafab17339cddc6f2
ARY <widhiarya@outlook.com>
2022-12-20 15:00:49 +0700

    Tugas day 26

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import * as path from "path";
+import {LoginPageComponent} from "./pages/login-page/login-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent},
+  { path: '', redirectTo: 'login', pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
