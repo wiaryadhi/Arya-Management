53862be502abae18753b17186cd8858d14d69ba8
ARY <widhiarya@outlook.com>
2022-12-20 16:16:01 +0700

    Bonus

---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {Observable} from "rxjs";
+import {LoginService} from "./login.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(private router: Router,
+              private loginService: LoginService) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
+    if (!this.loginService.isUserLoggedIn()){
+      alert('Silahkan login terlebih dahulu')
+      this.router.navigate(['login'], {queryParams: { lastURL: route.url}})
+      return false;
+    }
+    return true;
+  }
+}
